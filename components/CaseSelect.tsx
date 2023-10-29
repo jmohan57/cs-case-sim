1862c61af20323381d2bb63af615bebfd4066702
jmohan57 <jeffreymohan57@gmail.com>
2023-10-29 17:31:49 +0100

    CaseSelect: restyle select

--- components/CaseSelect.tsx
+++ components/CaseSelect.tsx
@@ -12,18 +12,24 @@ export default ({
   const router = useRouter();
   const caseParam = useSearchParams().get("case");
   const [playCaseSound] = useSound("/audio/caseselect.mp3");
+  const [playHover] = useSound("/audio/itemhover.mp3");
 
   return (
     <select
-      className="mb-0 max-w-fit rounded bg-[#048b59] p-2 tracking-wider text-white"
+      className="cursor-pointer rounded bg-transparent p-2 text-lg font-semibold tracking-wide backdrop-blur-md transition-colors duration-300 hover:bg-black/50 focus-visible:bg-black/50"
       defaultValue={caseParam ?? availableCases[0].id}
+      onMouseEnter={playHover}
       onChange={e => {
         playCaseSound();
         router.replace(`/?case=${e.target.value}`);
       }}
     >
       {availableCases.map(caseData => (
-        <option key={caseData.id} value={caseData.id}>
+        <option
+          key={caseData.id}
+          className="bg-[#2d2d2d] text-base"
+          value={caseData.id}
+        >
           {caseData.name}
         </option>
       ))}
