26c4bfaf0675a7f17bfe61987b462c0a9521b593
jmohan57 <jeffreymohan57@gmail.com>
2023-10-29 21:28:23 +0100

    update metadata link

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,29 +11,29 @@ const stratumFont = localFont({
     {
       path: "./Stratum2WebRegular.woff",
       weight: "400",
-      style: "normal"
+      style: "normal",
     },
     {
       path: "./Stratum2WebMedium.woff",
       weight: "500",
-      style: "medium"
+      style: "medium",
     },
     {
       path: "./Stratum2WebBold.woff",
       weight: "700",
-      style: "bold"
-    }
-  ]
+      style: "bold",
+    },
+  ],
 });
 
 export const metadata: Metadata = {
   title: "Counter-Strike Case Simulator",
   description: "Open all the Counter-Strike cases you want for free!",
-  metadataBase: new URL("https://case-sim.vercel.app")
+  metadataBase: new URL("https://case-sim.com"),
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
