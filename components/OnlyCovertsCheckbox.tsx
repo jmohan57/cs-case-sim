cb68072900aebcc0e176d9cef64dcdbc1b99f9e7
jmohan57 <jeffreymohan57@gmail.com>
2023-11-08 19:02:39 +0100

    unbocxed: add checkbox disabled state

--- components/OnlyCovertsCheckbox.tsx
+++ components/OnlyCovertsCheckbox.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+import { useTransition } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export default () => {
+  const [pending, startTransition] = useTransition();
   const onlyCoverts = useSearchParams().get("onlyCoverts") === "true";
 
   const router = useRouter();
   return (
     <div className="flex items-center gap-1">
       <input
-        className="h-4 w-4 accent-[#048b59]"
+        className="h-4 w-4 accent-[#048b59] disabled:cursor-not-allowed"
         type="checkbox"
         id="onlyCovertsCheckbox"
         defaultChecked={onlyCoverts}
+        disabled={pending}
         onChange={() => {
           if (onlyCoverts) return router.replace("?");
-          router.replace(`?onlyCoverts=${!onlyCoverts}`);
+          startTransition(() => router.replace(`?onlyCoverts=${!onlyCoverts}`));
         }}
       />
-      <label className="pt-[2px] text-lg" htmlFor="onlyCovertsCheckbox">
+      <label
+        className={`pt-[2px] text-lg ${pending ? "cursor-not-allowed" : ""}`}
+        htmlFor="onlyCovertsCheckbox"
+      >
         Show only coverts
       </label>
     </div>
