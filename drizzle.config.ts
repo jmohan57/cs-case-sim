aa0af89cafef2bb42e89afaca41e65b69684d4bf
jmohan57 <jeffreymohan57@gmail.com>
2024-03-23 19:03:17 +0100

    drizzle: update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,10 @@ export default {
   out: "./lib/db",
   schema: "./lib/db/schema.ts",
   dbCredentials: {
-    uri: process.env.DATABASE_URL!,
+    uri: process.env.DATABASE_URL!.replace(
+      "?sslaccept=strict",
+      '?ssl={"rejectUnauthorized":true}',
+    ),
   },
   tablesFilter: ["case_sim_items"],
   // Print all statements
