c11a98047a67bae0b2ecafc7ad2205e5269c9f5a
jmohan57 <jeffreymohan57@gmail.com>
2024-05-19 12:34:21 +0200

    cleaning

--- lib/db/deleteOldNonCoverts.ts
+++ lib/db/deleteOldNonCoverts.ts
@@ -2,23 +2,25 @@ import { planetscaleClient } from "./";
 
 const deleteOldNonCovertItems = async (batchSize: number) => {
   // Max 100k batchSize
-  if (batchSize > 100_000)
-    return console.error("Batch size cannot exceed 100,000.");
+  if (batchSize > 100_000) {
+    console.error("Batch size cannot exceed 100,000.");
+    return;
+  }
 
   // Query to delete items older than 2 months that are not covert items
-  const query = `DELETE FROM case_sim_items WHERE unboxed_at < DATE_SUB(CURRENT_DATE, INTERVAL 2 MONTH) AND rarity NOT IN ('Covert', 'Extraordinary') LIMIT ${batchSize}`;
+  const query =
+    "DELETE FROM case_sim_items WHERE unboxed_at < DATE_SUB(CURRENT_DATE, INTERVAL 2 MONTH) AND rarity NOT IN ('Covert', 'Extraordinary') LIMIT ?";
 
-  let continueDeleting = true;
   let totalRowsDeleted = 0;
   const operationStart = performance.now();
 
   // Loop until no more rows are deleted
-  while (continueDeleting) {
+  while (true) {
     const currentBatchStart = performance.now();
     console.log(`Deleting ${batchSize.toLocaleString("en")} rows...`);
 
     // Execute the query and add the number of rows deleted
-    const response = await planetscaleClient.execute(query);
+    const response = await planetscaleClient.execute(query, [batchSize]);
     totalRowsDeleted += response.rowsAffected;
     const currentBatchEnd = performance.now();
 
@@ -28,15 +30,15 @@ const deleteOldNonCovertItems = async (batchSize: number) => {
       ).toFixed()}ms`,
     );
 
-    // If less than the chosen batchSize amount of rows are deleted, there are no more rows to delete
+    // If fewer than the chosen batchSize amount of rows are deleted, there are no more rows to delete
     if (response.rowsAffected < batchSize) {
-      continueDeleting = false;
       const operationEnd = performance.now();
       console.log(
         `✓ Complete. No more rows to delete. Deleted ${totalRowsDeleted.toLocaleString(
           "en",
         )} rows in ${(operationEnd - operationStart).toFixed()}ms`,
       );
+      break;
     }
   }
 };
