8448d2631471a0afdc2f26fa89f853842b5c2aae
jmohan57 <jeffreymohan57@gmail.com>
2024-06-13 01:07:55 +0900

    some message > > Co-authored-by: smaster007 <171411576+SecretWai@users.noreply.github.com> Co-authored-by: SecretWai <171411576+SecretWai@users.noreply.github.com>

--- utils/getCasePrice.ts
+++ utils/getCasePrice.ts
@@ -2,8 +2,6 @@ import casePrices from "@/lib/data/casePrices.json";
 // Gathered at 2024-01-03
 
 export default (caseName: string): number | null => {
-  // casePrices is gotten from https://prices.csgotrader.app/latest/prices_v6.json
-  // -> Object.entries(data).map(([name, prices]) => ({ name, prices })).filter(x => (x.name.includes(" Case") || x.name.includes(" Package")) && (!x.name.includes("Hardened") && !x.name.includes("Key") && !x.name.includes("Collection")))
   const currentCasePrices = casePrices.find(x => x.name === caseName)?.prices;
 
   const currentCasePrice =
